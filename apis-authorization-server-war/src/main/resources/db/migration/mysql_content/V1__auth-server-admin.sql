c33cd137965020d464e96ea0461ddb3500e7df24
Amal Elshihaby <ashihaby@cloud9ers.com>
2014-08-25 11:46:30 +0300

    Modified oauth migration script

--- apis-authorization-server-war/src/main/resources/db/migration/mysql_content/V1__auth-server-admin.sql
+++ apis-authorization-server-war/src/main/resources/db/migration/mysql_content/V1__auth-server-admin.sql
@@ -15,3 +15,21 @@ VALUES
     'https://raw.github.com/OpenConextApps/apis/master/apis-images/surf-oauth-client.png', 99998,
     'authorization-server-admin-js-client', 1, 1);
 INSERT INTO Client_scopes values (99998, 'read'), (99998, 'write');
+
+/*
+Client for user password credentials
+*/
+INSERT INTO resourceserver (id, contactEmail,  contactName, resourceServerName, resourceServerKey, secret, owner, thumbNailUrl)
+VALUES
+	(99999, 'oauth@dewan.com','local admin','Dewan Authorization Server',
+	'authorization-server-admin', 'cafebabe-cafe-babe-cafe-babecafebabe', null, 'https://raw.github.com/OpenConextApps/apis/master/apis-images/surf-oauth.png');
+INSERT INTO ResourceServer_scopes values (99999, 'read'),(99999, 'write') ;
+
+INSERT INTO client (id, contactEmail, contactName, description, clientName, thumbNailUrl, resourceserver_id,
+clientId, secret, expireDuration, useRefreshTokens, allowedUserPasswordCredentials)
+VALUES
+    (99999, 'oauth@dewan.com', 'oauth.dewan', 'Web application for authorization',
+    'Dewan Oauth client',
+    'thumbnailurl', 99999,
+    'dewan-oauth-client', 'c2FsYW0ta2V5OnNhbGFtLX', 36000, 1, 1);
+INSERT INTO  Client_scopes values (99999, 'read'),(99999, 'write');
